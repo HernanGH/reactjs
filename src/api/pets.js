765176531e4293f18c2bb376d0ee4e7b96e373d0
Hernan Condori <hcondori@elementum.com>
2021-08-29 13:09:37 -0300

    Initial commit

---
+++ src/api/pets.js
@@ -0,0 +1,36 @@
+const pets = [
+  {
+    id: 0,
+    name: "Nina",
+    bread: "Caniche",
+    animal: "dog",
+    age: 7,
+    description: "Pequeña, pelo blanco muy inquieta",
+  },
+  {
+    id: 1,
+    name: "Homero",
+    bread: "Mestizo",
+    animal: "cat",
+    age: 3,
+    description: "Mediano, pelo negro y blanco, muy jugueton",
+  },
+  {
+    id: 2,
+    name: "Terry",
+    bread: "Ovejero Aleman",
+    animal: "dog",
+    age: 2,
+    description: "Grande, pelo negro y marron, muy tranquilo",
+  },
+  {
+    id: 3,
+    name: "Goliat",
+    bread: "Persa",
+    animal: "dog",
+    age: 4,
+    description: "Chico, pelo blanco, extremadamente tranquilo",
+  }
+];
+
+export default pets;
